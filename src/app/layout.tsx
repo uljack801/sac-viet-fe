ec78ba7dd00aa899ee021f9f56d1b79dde145747
TIEN\tienn <tiennq201@gmail.com>
2025-06-11 22:31:38 +0700

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,30 @@ import type { Metadata } from "next";
 import { AuthProvider } from "@/app/AuthContext";
 
 export const metadata: Metadata = {
-  title: "Sắc Việt",
-  description: "Kết nối tinh hoa thủ công Việt. Tôn vinh bản sắc, lan tỏa giá trị ",
-   icons: {
-    icon: "/logo_.png",
+  title: "Sắc Việt - Kết nối tinh hoa thủ công Việt",
+  description: "Sắc Việt kết nối nghệ nhân thủ công Việt, tôn vinh văn hoá, lan toả giá trị Việt ra thế giới.",
+  metadataBase: new URL("https://www.sacviet.info.vn"),
+  robots: "index, follow",
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    title: "Sắc Việt - Kết nối tinh hoa thủ công Việt",
+    description: "Sắc Việt kết nối nghệ nhân thủ công Việt, tôn vinh văn hoá, lan toả giá trị Việt ra thế giới.",
+    url: "https://www.sacviet.info.vn/",
+    siteName: "Sắc Việt",
+    images: [
+      {
+        url: "/logo_.png", // logo của bạn
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "vi_VN",
+    type: "website",
   },
 };
+
 
 export default function RootLayout({
   children,
